e88ae9b71241e90211d9061d78398d55b68aa37e
耘之 <yfliu98@outlook.com>
2018-07-16 15:36:06 +0800

    Initial Commit

---
+++ hw4/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hw4
+//
+//  Created by 刘逸丰 on 2018/7/16.
+//  Copyright © 2018年 W.Studio. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
